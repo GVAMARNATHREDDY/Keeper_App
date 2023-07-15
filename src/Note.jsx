c5aee1ed876e4d129b629cea701f424297b4b5a0
GVAMARNATHREDDY <95522905+GVAMARNATHREDDY@users.noreply.github.com>
2023-07-15 15:38:55 +0530

    Initial commit

---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <note>
+      <h1>This is the note title amar</h1>
+      <p>This is the note content</p>
+    </note>
+  );
+}
+
+export default Note;
